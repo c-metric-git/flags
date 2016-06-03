abbaf7945ce26881dd09195a9841be45e290fe50
c-metric-git <hardik.dwivedi@c-metric.com>
2016-06-03 07:04:38 -0400

    dhiraj changes added

--- lib/Teamdesk/engine_config.php
+++ lib/Teamdesk/engine_config.php
@@ -1,10 +1,6 @@
 <?php
 
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'stripeds_user');
-    define('DB_PASSWORD', 'S@t(d#S*K-S');
-    define('DB_NAME', 'stripeds_beta');
-    define('DB_PREFIX', '');
+	define('DB_PREFIX', '');
     ini_set('display_errors',0);
     define('TD_LIVE_FLAG',0);
     define('TD_DOMAIN','clownantics.teamdesk.net');
@@ -12,3 +8,16 @@
     define('TD_USERNAME', 'blake@clownantics.com');    
     define('TD_PASSWORD', 'julian202');    
     define('TD_UPDATE_MAIL_ID','dhiraj@clownantics.com');
+    
+    if($_SERVER['HTTP_HOST'] == 'http://amazon.flagsrus.org') {
+        define('DB_HOST', 'flagsrusnew.cuobrtussnxi.us-east-1.rds.amazonaws.com');
+        define('DB_USER', 'flagsrusnew');
+        define('DB_PASSWORD', 'harddisk');
+        define('DB_NAME', 'flagsrus');
+    }     
+    else {
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'stripeds_user');
+        define('DB_PASSWORD', 'S@t(d#S*K-S');
+        define('DB_NAME', 'stripeds_beta');
+    } 
